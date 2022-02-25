06df2806aa4adcf6baaedb29bf9a5087662ae4e2
Caio Vieira <contato@caiovieira.com.br>
2022-02-25 19:20:31 -0300

    feat: ensure Service calls LoadUSerAccountRepo when LoadFacebookUserApi returns data

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,18 +1,20 @@
 import { FacebookAuthentication } from '@/domain/features'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { AuthenticationError } from '@/domain/errors'
-import { LoadUserAccountRepository } from '@/data/contracts/repos'
+import { LoadUserAccountRepository, CreateFacebookAccountRepository } from '@/data/contracts/repos'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly loadFacebookUserByTokenApi: LoadFacebookUserApi,
-    private readonly loadUserAccountRepo: LoadUserAccountRepository
+    private readonly loadUserAccountRepo: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepo: CreateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.loadFacebookUserByTokenApi.loadUser(params)
     if (fbData !== undefined) {
       await this.loadUserAccountRepo.load({ email: fbData?.email })
+      await this.createFacebookAccountRepo.createFromFacebook(fbData)
     }
     return new AuthenticationError()
   }
